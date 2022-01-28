f634b8ab20038d786118ff669a7848e5ad3750cf
Michael Smethurst <michael@smethur.st>
2022-01-28 14:52:52 +0000

    new show action for reviews

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -6,4 +6,13 @@ class ReviewController < ApplicationController
       .select( 'reviews.published_on, models.id, scales.name as scale_name, manufacturers.name as manufacturer_name, operators.name as operator_name, locomotive_classes.name as locomotive_class_name' )
       .order( 'reviews.published_on desc' )
   end
+  
+  def show
+    review = params[:review]
+    @review = Review
+      .joins( :model, :detail_score, :performance_score, :haulage_capability, :mechanism_score, :quality_score, :value_score, :manufacturer, :scale, :operator, :locomotive_class )
+      .select( 'reviews.published_on, reviews.score, models.id, scales.name as scale_name, manufacturers.name as manufacturer_name, operators.name as operator_name, locomotive_classes.name as locomotive_class_name, detail_scores.score as inline_detail_score, performance_scores.score as inline_performance_score, haulage_capabilities.number_of_coaches as number_of_coaches, mechanism_scores.score as inline_mechanism_score, quality_scores.score as inline_quality_score, value_scores.score as inline_value_score' )
+      .where( "reviews.id = #{review}")
+      .first
+  end
 end
