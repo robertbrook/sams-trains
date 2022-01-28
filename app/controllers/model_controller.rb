967d3fa191b25b95c46f7d0c7bf4a7905ff20190
Michael Smethurst <michael@smethur.st>
2022-01-28 16:43:19 +0000

    new show view for models

--- app/controllers/model_controller.rb
+++ app/controllers/model_controller.rb
@@ -6,4 +6,13 @@ class ModelController < ApplicationController
       .select( 'models.id, scales.name as scale_name, manufacturers.name as manufacturer_name, operators.name as operator_name, locomotive_classes.name as locomotive_class_name' )
       .order( 'scale_name, manufacturer_name, operator_name, locomotive_class_name' )
   end
+  
+  def show
+    model = params[:model]
+    @model = Model
+      .joins( :manufacturer, :scale, :operator, :livery, :locomotive_class )
+      .select( 'models.id, scales.name as scale_name, manufacturers.name as manufacturer_name, operators.name as operator_name, liveries.name as livery_name, locomotive_classes.name as locomotive_class_name' )
+      .where( "models.id = #{model}" )
+      .first
+  end
 end
