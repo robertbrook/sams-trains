43c9b2b107923e8a303f017df15f5ae535ca3f34
Michael Smethurst <michael@smethur.st>
2022-01-28 11:42:04 +0000

    added associations to scores

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   
   belongs_to :model
+  belongs_to :haulage_capability
+  belongs_to :detail_score
+  belongs_to :performance_score
+  belongs_to :mechanism_score
+  belongs_to :quality_score
+  belongs_to :value_score
 end
