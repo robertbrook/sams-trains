adb671d4cc79d28f5644349b8fdaaa926eebfeaf
Michael Smethurst <michael@smethur.st>
2022-01-28 18:54:27 +0000

    show query now returns id of scale to make link from model details partial

--- app/controllers/model_controller.rb
+++ app/controllers/model_controller.rb
@@ -11,7 +11,7 @@ class ModelController < ApplicationController
     model = params[:model]
     @model = Model
       .joins( :manufacturer, :scale, :operator, :livery, :locomotive_class )
-      .select( 'models.id, scales.name as scale_name, manufacturers.id as manufacturer_id, manufacturers.name as manufacturer_name, operators.name as operator_name, liveries.name as livery_name, locomotive_classes.name as locomotive_class_name' )
+      .select( 'models.id, scales.id as scale_id, scales.name as scale_name, manufacturers.id as manufacturer_id, manufacturers.name as manufacturer_name, operators.name as operator_name, liveries.name as livery_name, locomotive_classes.name as locomotive_class_name' )
       .where( "models.id = #{model}" )
       .first
   end
