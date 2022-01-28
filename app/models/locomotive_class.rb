ceb6f7015d72d09d88b2d1d2e76b724c8a4fe2f2
Michael Smethurst <michael@smethur.st>
2022-01-28 19:47:49 +0000

    methods to find associated models, add wikipedia and wikidata links

--- app/models/locomotive_class.rb
+++ app/models/locomotive_class.rb
@@ -1,2 +1,25 @@
 class LocomotiveClass < ApplicationRecord
+  
+  def models
+    Model.find_by_sql(
+      "
+        SELECT m.*, s.name as scale_name, man.name as manufacturer_name, o.name as operator_name, l.name as locomotive_class_name
+        FROM models m, scales s, manufacturers man, operators o, locomotive_classes l
+        WHERE m.scale_id = s.id
+        AND m.manufacturer_id = man.id
+        AND m.operator_id = o.id
+        AND m.locomotive_class_id = l.id
+        AND l.id = #{self.id}
+        ORDER BY scale_name, manufacturer_name, locomotive_class_name, operator_name
+      "
+    )
+  end
+  
+  def wikidata_url
+    "https://www.wikidata.org/wiki/" + self.wikidata_id
+  end
+  
+  def wikipedia_url
+    "https://www.wikidata.org/wiki/Special:GoToLinkedPage/enwiki/" + self.wikidata_id
+  end
 end
