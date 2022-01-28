ff4f224d49080ebbe134550447bddfac55bdf8e1
Michael Smethurst <michael@smethur.st>
2022-01-28 18:53:35 +0000

    method to get models for a scale

--- app/models/scale.rb
+++ app/models/scale.rb
@@ -1,2 +1,17 @@
 class Scale < ApplicationRecord
+  
+  def models
+    Model.find_by_sql(
+      "
+        SELECT m.*, s.name as scale_name, man.name as manufacturer_name, o.name as operator_name, l.name as locomotive_class_name
+        FROM models m, scales s, manufacturers man, operators o, locomotive_classes l
+        WHERE m.scale_id = s.id
+        AND m.manufacturer_id = man.id
+        AND m.operator_id = o.id
+        AND m.locomotive_class_id = l.id
+        AND s.id = #{self.id}
+        ORDER BY scale_name, manufacturer_name, locomotive_class_name, operator_name
+      "
+    )
+  end
 end
