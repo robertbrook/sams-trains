d5e4be1c4e75119364081efbf961333eace34e5e
Michael Smethurst <michael@smethur.st>
2022-01-28 18:00:51 +0000

    brings back overal score on review

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -3,7 +3,7 @@ class ReviewController < ApplicationController
   def index
     @reviews = Review
       .joins( :model ).joins( :manufacturer, :scale, :operator, :locomotive_class )
-      .select( 'reviews.published_on, models.id, scales.name as scale_name, manufacturers.name as manufacturer_name, operators.name as operator_name, locomotive_classes.name as locomotive_class_name' )
+      .select( 'reviews.published_on, reviews.score, models.id, scales.name as scale_name, manufacturers.name as manufacturer_name, operators.name as operator_name, locomotive_classes.name as locomotive_class_name' )
       .order( 'reviews.published_on desc' )
   end
   
