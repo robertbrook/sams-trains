fc33ba6b2377b35791aff4db961cb1120058e3bd
Michael Smethurst <michael@smethur.st>
2022-01-28 15:01:58 +0000

    include youtube url:

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -11,8 +11,8 @@ class ReviewController < ApplicationController
     review = params[:review]
     @review = Review
       .joins( :model, :detail_score, :performance_score, :haulage_capability, :mechanism_score, :quality_score, :value_score, :manufacturer, :scale, :operator, :locomotive_class )
-      .select( 'reviews.published_on, reviews.score, models.id, scales.name as scale_name, manufacturers.name as manufacturer_name, operators.name as operator_name, locomotive_classes.name as locomotive_class_name, detail_scores.score as inline_detail_score, performance_scores.score as inline_performance_score, haulage_capabilities.number_of_coaches as number_of_coaches, mechanism_scores.score as inline_mechanism_score, quality_scores.score as inline_quality_score, value_scores.score as inline_value_score' )
-      .where( "reviews.id = #{review}")
+      .select( 'reviews.published_on, reviews.score, reviews.youtube_url, models.id, scales.name as scale_name, manufacturers.name as manufacturer_name, operators.name as operator_name, locomotive_classes.name as locomotive_class_name, detail_scores.score as inline_detail_score, performance_scores.score as inline_performance_score, haulage_capabilities.number_of_coaches as number_of_coaches, mechanism_scores.score as inline_mechanism_score, quality_scores.score as inline_quality_score, value_scores.score as inline_value_score' )
+      .where( "reviews.id = #{review}" )
       .first
   end
 end
