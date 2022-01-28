10e85863582db640ea21814e49765b5a20f2c336
Michael Smethurst <michael@smethur.st>
2022-01-28 13:40:50 +0000

    added has many association to reviews, new title method

--- app/models/model.rb
+++ app/models/model.rb
@@ -5,4 +5,9 @@ class Model < ApplicationRecord
   belongs_to :manufacturer
   belongs_to :livery
   belongs_to :locomotive_class
+  has_many :reviews, -> { order 'published_on' }
+  
+  def title
+    self.scale_name + ' ' +self.manufacturer_name + ' ' + self.operator_name + ' ' + self.locomotive_class_name
+  end
 end
