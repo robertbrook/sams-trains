45d2d55b77b645b0c2b71b9f8bde8d4c03a6a65c
Michael Smethurst <michael@smethur.st>
2022-01-30 14:13:04 +0000

    livery show query left joins to operator to save a query

--- app/controllers/livery_controller.rb
+++ app/controllers/livery_controller.rb
@@ -6,6 +6,17 @@ class LiveryController < ApplicationController
   
   def show
     livery = params[:livery]
-    @livery = Livery.find( livery )
+    @livery = Livery.find_by_sql(
+      "
+        SELECT l.*, operator.operator_id AS operator_id, operator.operator_name AS operator_name
+        FROM liveries l
+        LEFT JOIN (
+          SELECT o.id AS operator_id, o.name AS operator_name
+          FROM operators o
+        ) operator
+        ON operator.operator_id = l.operator_id
+        WHERE l.id = #{livery}
+      "
+    ).first
   end
 end
