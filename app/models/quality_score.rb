5118e818008510a576426f7f503bb29dc78882ba
Michael Smethurst <michael@smethur.st>
2022-01-30 22:02:08 +0000

    added method to get reviews

--- app/models/quality_score.rb
+++ app/models/quality_score.rb
@@ -1,2 +1,10 @@
 class QualityScore < ApplicationRecord
+  
+  def reviews
+    @reviews = Review
+      .joins( :model ).joins( :manufacturer, :scale, :operator, :locomotive_class )
+      .select( 'reviews.published_on, reviews.score, models.id, scales.name as scale_name, manufacturers.name as manufacturer_name, operators.name as operator_name, locomotive_classes.name as locomotive_class_name' )
+      .where( "reviews.quality_score_id = #{self.id}" )
+      .order( 'reviews.published_on desc' )
+    end
 end
