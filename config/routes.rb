b5cf4692e6de04d705832aad6f7591119bb53f6d
Michael Smethurst <michael@smethur.st>
2022-01-31 10:29:06 +0000

    added name on home url

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 
-  root 'home#index'
+  root 'home#index', as: :home
   
   get 'reviews' => 'review#index', as: :review_list
   get 'reviews/:review' => 'review#show', as: :review_show
