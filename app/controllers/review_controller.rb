a7eceb9998c35530a91c7a6695e22303f466f134
Michael Smethurst <michael@smethur.st>
2022-02-02 22:24:58 +0000

    added grid view for reviews

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -7,6 +7,13 @@ class ReviewController < ApplicationController
       .order( 'reviews.published_on desc' )
   end
   
+  def grid
+    @reviews = Review
+      .joins( :model, :detail_score, :performance_score, :mechanism_score ).joins( :manufacturer, :scale, :operator, :locomotive_class, :livery,  )
+      .select( 'reviews.*, models.id, scales.id as scale_id, scales.name as scale_name, manufacturers.id as manufacturer_id, manufacturers.name as manufacturer_name, operators.id as operator_id, operators.name as operator_name, locomotive_classes.id as locomotive_class_id, locomotive_classes.name as locomotive_class_name, liveries.id as livery_id, liveries.name as livery_name, detail_scores.id as detail_score_id, detail_scores.score as detail_score_score, performance_scores.id as performance_score_id, performance_scores.score as performance_score_score, mechanism_scores.id as mechanism_score_id, mechanism_scores.score as mechanism_score_score' )
+      .order( 'reviews.published_on desc' )
+  end
+  
   def show
     review = params[:review]
     @review = Review
